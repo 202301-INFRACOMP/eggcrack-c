f79c296e58cc9dc214bcc6dbc29891b8c207e911
orion160 <idbl64@outlook.com>
2023-08-08 10:13:45 -0500

    Add startup banner

--- src/main.c
+++ src/main.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 
-#include <factory/id_sequencer.h>
-
 int main(int argc, char **argv)
 {
-    id_sequencer *s = sequencer_create();
+    printf("========================================================================\n"
+           "                                       _                                \n"
+           "                                      | |                               \n"
+           "   ___  __ _  __ _  ___ _ __ __ _  ___| | __                            \n"
+           "  / _ \\/ _` |/ _` |/ __| '__/ _` |/ __| |/ /                           \n"
+           " |  __/ (_| | (_| | (__| | | (_| | (__|   <                             \n"
+           "  \\___|\\__, |\\__, |\\___|_|  \\__,_|\\___|_|\\_\\                    \n"
+           "        __/ | __/ |                                                     \n"
+           "       |___/ |___/                                                      \n"
+           "========================================================================\n\n");
 
-    printf("%zd\n", sequencer_get_id(s));
-    printf("%zd\n", sequencer_get_id(s));
-    printf("%zd\n", sequencer_get_id(s));
-    printf("%zd\n", sequencer_get_id(s));
+    printf("Welcome to eggcrack - C implementation\n");
+    printf("For more information see: https://github.com/202301-INFRACOMP/eggcrack-c\n");
 
-    sequencer_destroy(s);
+    printf("\nWork finished!!!\n");
+    printf("Press enter to exit...\n");
+
+    while (getchar() != '\n')
+    {
+    }
 
     return 0;
 }
